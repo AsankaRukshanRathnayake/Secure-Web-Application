ae9fca1d5ff550b6350d5539f8a6448bc8acb5d2
AsankaRukshanRathnayake <rathnay-se19053@stu.kln.ac.lk>
2023-10-10 00:31:29 +0530

    Modified so that displayReservation() function returns a list

--- src/main/java/com/vehicleserviceapp/controller/Crud.java
+++ src/main/java/com/vehicleserviceapp/controller/Crud.java
@@ -1,33 +1,47 @@
 package com.vehicleserviceapp.controller;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Map;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 import java.sql.ResultSet;
 
 import com.vehicleserviceapp.model.DatabaseConnection;
 
 public class Crud {
-	public static Map<String, String> displayReservations(String username) throws ClassNotFoundException, SQLException{
-		Connection connection = DatabaseConnection.initializeDatabase();
-		
-		Statement st = connection.createStatement();
-		ResultSet rs = st.executeQuery("select date, time, location, vehicle_no, mileage, message from user where username='"+username+"'");
-		
-		 Map<String, String> dataMap = new HashMap<>();
-		 
-		 while (rs.next()) {
-			 String date=rs.getString("date");
-			 String time=rs.getString("time");
-			 String location=rs.getString("location");
-			 String vehicle_no=rs.getString("vehicle_no");
-			 String mileage=rs.getString("mileage");
-			 String message=rs.getString("message");
-		 }
-		 
-		 return dataMap;
-	}
+    public static List<String[]> displayReservations(String username) throws ClassNotFoundException, SQLException {
 
+    	Connection connection = DatabaseConnection.initializeDatabase();
+
+        List<String[]> result = new ArrayList<>();
+
+        if (connection != null) {
+            try {
+                PreparedStatement st = connection.prepareStatement("SELECT booking_id, date, time, location, vehicle_no, mileage, message FROM vehicle_service WHERE username = ?");
+                st.setString(1, username);
+                ResultSet rs = st.executeQuery();
+
+                while (rs.next()) {
+                	String booking_id=rs.getString("booking_id");
+                    String date = rs.getString("date");
+                    String time = rs.getString("time");
+                    String location = rs.getString("location");
+                    String vehicle_no = rs.getString("vehicle_no");
+                    String mileage = rs.getString("mileage");
+                    String message = rs.getString("message");
+
+                    String[] rowData = { booking_id, date, time, location, vehicle_no, mileage, message };
+                    result.add(rowData);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                connection.close();
+            }
+        }
+
+        return result;
+    }
 }
