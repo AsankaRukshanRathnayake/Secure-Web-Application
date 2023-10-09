9e32918675d8e28c76ad50d849998522d2f8877d
AsankaRukshanRathnayake <rathnay-se19053@stu.kln.ac.lk>
2023-10-10 00:29:11 +0530

    Modified connection file

--- src/main/java/com/vehicleserviceapp/model/DatabaseConnection.java
+++ src/main/java/com/vehicleserviceapp/model/DatabaseConnection.java
@@ -7,12 +7,8 @@ import java.sql.SQLException;
 // This class can be used to initialize the database connection 
 public class DatabaseConnection { 
     public static Connection initializeDatabase() throws SQLException, ClassNotFoundException {
-    	try {
-    		Class.forName("com.mysql.jdbc.Driver");
-        	Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/isec_assessment2","root","");
-        	return connection;
-    	}catch(Exception e) {
-    		return null;
-    	}
+    	Class.forName("com.mysql.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/isec_assessment2","root","");
+        return connection;
     } 
 } 
